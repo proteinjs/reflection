f53640589efcbb0c85643c357606cc0f989771af
Brent Bahry <brentbahry@gmail.com>
2024-04-26 17:09:11 -0700

    fix: `runWatch` now requires gulp with `require.resolve`. when reflection-build is published, its node_modules are not included. therefore reflection-build (`runWatch` in this case) can't require with a relative path into its own node_modules/

--- packages/reflection-build/src/runWatch.ts
+++ packages/reflection-build/src/runWatch.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 import { cmd } from '@proteinjs/util-node';
 
 // Determine the paths to gulp and the src/watch.ts
-const gulpPath = path.resolve(__dirname, '../../node_modules/.bin/gulp');
+const gulpPath = require.resolve('gulp/bin/gulp.js');
 const gulpfilePath = path.resolve(__dirname, './watch.js');
 
 // Run the Gulp script
