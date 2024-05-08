162365e94d8f306b69351f5cc071a732cab5745c
Brent Bahry <brentbahry@gmail.com>
2024-05-07 19:13:23 -0700

    fix `SourceRepositoryTypeFilter` tests

--- packages/reflection-build/test/SourceRepositoryTypeFilter.test.ts
+++ packages/reflection-build/test/SourceRepositoryTypeFilter.test.ts
@@ -7,18 +7,18 @@ beforeAll(async () => {
 
 test('filter foreign type', async () => {
 	const sourceRepository = SourceRepository.get();
-	const extendsLoadableForeignInterface = sourceRepository.flattenedSourceGraph.typeAliases['@proteinjs/reflection-build-test-b/OptionalLoadableForeignType'];
-	expect(extendsLoadableForeignInterface).toBeFalsy();
+  expect(() => sourceRepository.baseChildren('@proteinjs/reflection-build-test-b/OptionalLoadableForeignType')).toThrow();
+  expect(() => sourceRepository.directChildren('@proteinjs/reflection-build-test-b/OptionalLoadableForeignType')).toThrow();
 });
 
 test('filter foreign interface', async () => {
 	const sourceRepository = SourceRepository.get();
-	const extendsLoadableForeignInterface = sourceRepository.flattenedSourceGraph.interfaces['@proteinjs/reflection-build-test-b/OptionalLoadableForeignInterface'];
-	expect(extendsLoadableForeignInterface).toBeFalsy();
+  expect(() => sourceRepository.baseChildren('@proteinjs/reflection-build-test-b/OptionalLoadableForeignInterface')).toThrow();
+  expect(() => sourceRepository.directChildren('@proteinjs/reflection-build-test-b/OptionalLoadableForeignInterface')).toThrow();
 });
 
 test('filter foreign abstract class', async () => {
 	const sourceRepository = SourceRepository.get();
-	const extendsLoadableForeignInterface = sourceRepository.flattenedSourceGraph.classes['@proteinjs/reflection-build-test-b/OptionalLoadableForeignAbstractClass'];
-	expect(extendsLoadableForeignInterface).toBeFalsy();
+  expect(() => sourceRepository.baseChildren('@proteinjs/reflection-build-test-b/OptionalLoadableForeignAbstractClass')).toThrow();
+  expect(() => sourceRepository.directChildren('@proteinjs/reflection-build-test-b/OptionalLoadableForeignAbstractClass')).toThrow();
 });
