d7bdf6baa50f93781e71abac3c2732e87cb21999
Veronica Valenzuela <vmvalenzuela24@gmail.com>
2024-05-10 16:29:55 -0700

    fix: add .md file type to lint ignore files

--- packages/reflection-build/.eslintrc.js
+++ packages/reflection-build/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
   root: true,
-  ignorePatterns: ['**/dist/*', '**/node_modules/*', 'typescript-parser/'],
+  ignorePatterns: ['**/dist/*', '**/node_modules/*', 'typescript-parser/', '*.md'],
 
   rules: {
     'prettier/prettier': ['warn'],
